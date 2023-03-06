12d1aa0af22a537c5148394a6f9aa3b9bb9aa90e
gabeust <romero.gabriel.e@hotmail.com>
2023-03-06 14:38:24 -0300

    modificacion

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ let hideText = document.getElementById('hideText');
 hideText_btn.addEventListener('click', toggleText);
 
 function toggleText() {
-    hideText.classList.toggle('show');
+    hideText.classList.toggle ('show');
 
-}
-if (hideText.classList.contains('show')){
-    hideText_btn.innerHTML = 'Leer menos';
+
+if (hideText.classList.contains ('show')) {
+    hideText_btn.innerHTML= 'leer menos';
 }
  else {
-    hideText_btn.innerHTML = 'Leer mas';
+    hideText_btn.innerHTML= 'Leer mas';
     }
+}
+
+function vermas(id){
+    if(id=="mas"){
+    document.getElementById("desplegar").style.display="block";   
+    document.getElementById("mas").style.display="none"; 
+    }
+    else{
+    document.getElementById("desplegar").style.display="none";
+    document.getElementById("mas").style.display="inline";
+    }
+    }
+
+    
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+        btnSwitch.classList.toggle('active');
+    });
 
